13700d7a768f3a3b841e09e58699b5f67194f4ef
Jose David Carranza Angarita <jodacan2005@hotmail.com>
2023-11-15 12:18:05 -0500

    Creamos la primera fase del modelo funcional de un analizador de sentimientos de odio o de amor programado en C# utilizando un modelo de Machine Learning

---
+++ sentimentAnalysis/Program.cs
@@ -0,0 +1,16 @@
+﻿using SentimentAnalysis;
+
+//Load sample data
+var sampleData = new MLModel1.ModelInput()
+{
+    Rev_id = 2.429755E+08F,
+    Comment = @"you are very nice",
+    Logged_in = false,
+    Ns = @"article",
+    Sample = @"random",
+    Split = @"train",
+};
+
+//Load model and predict output
+var result = MLModel1.Predict(sampleData);
+Console.WriteLine(result.PredictedLabel);
